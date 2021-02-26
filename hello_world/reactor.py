7439020fc3ed18914f59f5c05958435985ce61f2
Ethan Ho <eho@tacc.utexas.edu>
2021-02-26 14:59:18 -0700

    Up to date example reactor
    Initial commit for up to date hello-world reactor. Passing with Tapis
    CLI v1.0.3

--- hello_world/reactor.py
+++ hello_world/reactor.py
@@ -1,2 +1,16 @@
+from reactors.runtime import Reactor
 
-print("Hello, world")
+
+def main():
+    r = Reactor()
+    r.logger.info("Actor received message: {}".format(r.context['raw_message']))
+    r.logger.debug("This is a DEBUG message from actor {}".format(r.uid))
+    r.logger.info("This is an INFO message from actor {}".format(r.uid))
+    r.logger.warning("This is a warning from actor {}".format(r.uid))
+    r.logger.info("Here's that secret value: {}".format(r.context.dont_reveal))
+    r.logger.info("Here's that value from the config.yml: {}".format(
+        r.settings.do_reveal))
+
+
+if __name__ == '__main__':
+    main()
