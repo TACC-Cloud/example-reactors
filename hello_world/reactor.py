82b2273013431acfe8b50e0849c3ab6a5c1a7c74
Ethan Ho <eho@tacc.utexas.edu>
2021-03-01 13:56:16 -0700

    Basic check for WORK filesystem

--- hello_world/reactor.py
+++ hello_world/reactor.py
@@ -1,13 +1,11 @@
+import os
 from reactors.runtime import Reactor
 from reactors.version import version as reactors_version
 
 
 def main():
     r = Reactor()
-    print(reactors_version)
-    print(dir(r))
     r.validate()
-
     r.logger.info("Actor received message: {}".format(r.context['raw_message']))
     r.logger.debug("This is a DEBUG message from actor {}".format(r.uid))
     r.logger.info("This is an INFO message from actor {}".format(r.uid))
@@ -15,6 +13,17 @@ def main():
     r.logger.info("Here's that secret value: {}".format(r.context.dont_reveal))
     r.logger.info("Here's that value from the config.yml: {}".format(
         r.settings.do_reveal))
+    r.logger.info(f"Using python-reactors version {reactors_version}")
+
+    # check filesystem availability
+    paths = ['/work/projects']
+    avail = {p: os.path.exists(p) for p in paths}
+    if all(avail.values()):
+        r.on_success(f"all checked paths exist")
+    else:
+        unavail = [p for p in avail if not avail[p]]
+        r.on_failure(f"The following paths do not exist: {unavail}")
+
 
 
 if __name__ == '__main__':
